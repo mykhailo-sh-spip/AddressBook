f1b737c805ca1a85329ee2e6b1019c6d4cb41404
Mykhailo Shapkin <72484542+mykhailo-sh-spip@users.noreply.github.com>
2022-01-05 11:51:44 +0000

    added email validation, changed menu to have a sub branch for look up

--- Contact.java
+++ Contact.java
@@ -1,9 +1,7 @@
-/*
-re-utilised Student class for getting the contact info and printing all contacts
-
-*/
 import java.text.*;
 import java.util.*;
+import java.util.regex.*;
+import java.io.*;
 
 public class Contact {
     Scanner scan = new Scanner(System.in);
@@ -12,38 +10,105 @@ public class Contact {
     static ArrayList<String> email = new ArrayList<>();
     static ArrayList<String> birthday = new ArrayList<>();//we make it static to be able to use it in all parts of program
 
-    public static void getFileData(String[] contact){//this method is for reading from file when we start the program
+    public static void getFileData(String[] contact){//this method is for reading from file (challenge 1)...not completed yet
         name.add(contact[0]);
         phone.add(contact[1]);
         email.add(contact[2]);
         birthday.add(contact[3]);//convert student score to int and store in int arrayList type
     }
 
-    public static void getValidateBirthday(){
+    public static void getValidateName(){ //menu option 1 pt1
+      String validName = null;
+      String inputString;
+      while (validName == null)
+      {
+        inputString = IOUtils.getString("Enter name: ");
+        if (inputString.length() >= 3){
+          validName = inputString;
+          } else {
+          System.out.println("Please enter at least 3 characters");
+          }
+      }
+      name.add(validName);
+    }
+
+    public static void getValidateEmail(){
+      String validEmail = null;
+      String inputString;
+      while (validEmail == null)
+      {
+        inputString = IOUtils.getString("Type email here: ");
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(inputString);
+        if (matcher.matches())
+            validEmail = inputString;
+        else
+            System.out.println("Please enter a valid email address");
+      }
+      email.add(validEmail);
+    }
+
+
+    public static boolean checkDigits(String stringToCheck){//auxillary to phone number validation
+      boolean bAllDigits = true;
+      int n = stringToCheck.length();
+      for (int i = 0; i < n; i++)
+      {
+          if (!Character.isDigit(stringToCheck.charAt(i)))
+          {
+        bAllDigits = false;
+        break;
+          }
+      }
+      return bAllDigits;
+    }
+    public static void getValidatePhoneNumber(){//menu option 1 pt3
+      String validPhone = null;
+      String inputString;
+      while (validPhone == null)
+      {
+          inputString = IOUtils.getString("Enter phone number (10 digits): ");
+          if (inputString.length()  != 10)
+        System.out.println("Phone number must be 10 digits");
+          else
+          {
+            if (checkDigits(inputString))
+              validPhone = inputString;
+            else
+              System.out.println("Phone number must be all digits");
+          }
+      }
+      phone.add(validPhone);//instead of return phone we just add validated phone value straight into our "phone" array list 
+    }
+
+    public static void getValidateBirthday(){ //menu option 1 pt4
       SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-      //To make strict date format validation
-      formatter.setLenient(false);
+      formatter.setLenient(false);//To make strict date format validation
       Date parsedDate = null;
+
       loopBD: while (parsedDate==null){
       try {
           String dateInput = IOUtils.getString("enter birthday date in format dd/mm/yyyy: ");
           parsedDate = formatter.parse(dateInput);
-          System.out.println("++validated DATE TIME ++"+formatter.format(parsedDate));
+          //System.out.println("++validated DATE TIME ++"+formatter.format(parsedDate));
 
       } catch (ParseException e) {
           System.out.println("Invalid date format. Try input again");
           continue loopBD;
       } 
+
       String strDate = formatter.format(parsedDate);//convert date to String format
       birthday.add(strDate);//add converted date string to array
       }
     }
 
-    public static 
+
     
-    public static void printInfo(int contactCount){//option 5 in address book menu
+    public static void printInfo(int contactCount){//menu option 5
         for (int i=0; i<contactCount; i++){
-            System.out.format("%24s%28s%12s%n", "Name: "+name.get(i), name.get(i)+" "+phone.get(i),"Email: "+email.get(i));
+            //24s for name, 16s for phone number, 23s for email, 15s for birthday
+            System.out.format("%24s%16s%23s%15s%n", "Name: "+name.get(i), " Phone number:"+phone.get(i)," Email: "+email.get(i), " BD: "+birthday.get(i));
         }
     }
 }
--- Main.java
+++ Main.java
@@ -19,29 +19,44 @@ public class Main{
     public static void mainMenu(){
         System.out.println("----------------------");
         System.out.println("1.Add a new contact. ");
-        System.out.println("2.Look up the contact based on name");
-        System.out.println("3.Delete a contact(after look up). ");//first will execute look up
-        System.out.println("4.Modify a contact(after look up). ");
-        System.out.println("5.Print all contacts ");
-        System.out.println("6.Exit ");
+        System.out.println("2.Look up the contact based on name");//after look up we will ask "what do you want to do with contact you found? delete / modify / do nothing(go back)"
+        //look up is going to loop through array of names and every time it finds the name it will record that name into separate array together with this name's index (number in array)
+        /*example list: 
+        [index 0] misha 1234567890 misha@misha.com 1/1/1
+        [index 1] misha 0987654321 misha@gmail.com 2/2/2
+        [index 2] toi eik 1234567890 toiek@gmail.com 3/3/3
+        we execute look up function--
+        type name you want to search for: misha
+        searching......
+        
+        */
+        System.out.println("3.Print all contacts ");
+        System.out.println("4.Exit ");
 
     }
 
     public static void main(String[] args) {
         int contactCount = 0;
-        loop1: while (true){
+        loop1: while (true){//while loop is given custom label "loop1"
           mainMenu();
           String choice = IOUtils.getString("What do you want to do?: ");
           switch (choice){
             case "1": //adding new contact
-              System.out.println("here we will have code for adding contacts");
+              Contact.getValidateName();
+              Contact.getValidatePhoneNumber();
+              Contact.getValidateEmail();
               Contact.getValidateBirthday();
-              
+              System.out.println("contact added successfully");
               contactCount++;
               continue loop1;
-            case "6":
+            case "2":
+
+            case "3": //print all contacts
+              Contact.printInfo(contactCount);
+            case "4":
+              //after we do file challenge before exit we will also have to write the changes to the file
               break loop1;
-            default:
+            default: //works like else for switch
               System.out.println("Invalid input, please try again");
               continue loop1;
           }
